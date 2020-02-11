88e84d6f7322d8ea27ec08db6016dd9346adb0b8
Connor Reese <connor.reese13@gmail.com>
2020-02-11 20:42:21 +0800

    Currency Project complete

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("Hello World");
+let amt = 0;
+let from = "USD";
+let to = "EUR";
+
+const setAmount = element => {
+  amt = element.value;
+  console.log(amt);
+};
+
+const setFromCurrency = element => {
+  from = element.value;
+  console.log(from);
+};
+
+const setToCurrency = element => {
+  to = element.value;
+  console.log(to);
+};
+
+const convert = () => {
+  axios
+    .get(`http://localhost:4000/test?base=${from}&symbols=${to}&amt=${amt}`)
+    .then(response => {
+      document.querySelector("#output").innerHTML = response.data.amt;
+    });
+};
